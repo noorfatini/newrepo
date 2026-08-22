edcc3ea35cf0d8b68296a4d445175fdcd626c547
NOORFATINI FARHAH BINTI NOOR ADLY <noorfatinifarhah@graduate.utm.my>
2026-08-22 21:26:45 +0800

    Add Ex2 answers

---
+++ src/main/java/com/example/AssetTracker/repository/TicketRepository.java
@@ -0,0 +1,10 @@
+package com.example.AssetTracker.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.example.AssetTracker.model.Ticket;
+
+public interface TicketRepository extends MongoRepository<Ticket, String> {
+    // Custom query methods can be defined here if needed
+
+}
